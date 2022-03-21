f83330fd17b3eb1799c3aa9594e356c757ca517d
user <user@DESKTOP-6JBEF5K>
2022-03-21 09:51:40 +0800

    test

---
+++ ConsoleApp1/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Car
+    {
+        string color = "red";
+        int cc = 1500;
+        string brand = "bmw";
+
+        public void Star()
+        {
+            Console.WriteLine("引擎發動");
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("車色：" + color);
+        }
+    }
+}
